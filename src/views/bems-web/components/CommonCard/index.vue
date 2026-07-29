ea7a5b534509dedf338f397d72d44b8ce1663ae2
han_dong_xue <han_dong_xue@noreply.gitcode.com>
2026-07-29 15:18:06 +0800

    公共组件通用卡片文件

---
+++ src/views/bems-web/components/CommonCard/index.vue
@@ -0,0 +1,16 @@
+<!--
+ * @component CommonCard
+ * @description 总览仪表盘通用卡片可跳转路由
+ -->
+<template>
+  <div class="quick-links">
+    <!-- 通用卡片 -->
+  </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({ name: 'CommonCard' })
+</script>
+
+<style scoped lang="less">
+</style>
